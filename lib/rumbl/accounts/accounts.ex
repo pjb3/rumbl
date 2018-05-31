b226eb79ef3414be5e232712e9a3ca331ccfba77
Paul Barry <mail@paulbarry.com>
2018-05-31 09:27:00 -0700

    Completed Chapter 4

---
+++ lib/rumbl/accounts/accounts.ex
@@ -0,0 +1,30 @@
+defmodule Rumbl.Accounts do
+  @moduledoc """
+  The Accounts context.
+  """
+
+  alias Rumbl.Accounts.User
+  alias Rumbl.Repo
+
+  def list_users do
+    Repo.all(User)
+  end
+
+  def get_user(id) do
+    Repo.get(User, id)
+  end
+
+  def get_user_by(params) do
+    Repo.get_by(User, params)
+  end
+
+  def change_user(%User{} = user) do
+    User.changeset(user, %{})
+  end
+
+  def create_user(attrs \\ %{}) do
+    %User{}
+    |> User.changeset(attrs)
+    |> Repo.insert()
+  end
+end
